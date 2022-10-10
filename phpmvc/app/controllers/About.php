967de87aef62c27f63525ab3241500323d915e3c
Andrie <Andriefirmansyah334@gmail.com>
2022-10-10 18:40:28 +0700

    View

--- phpmvc/app/controllers/About.php
+++ phpmvc/app/controllers/About.php
@@ -1,13 +1,22 @@
 <?php
 
-class About {
-    public function index($nama = 'Andrie', $pekerjaan = 'mahasiswa')
+class About extends Controller {
+    public function index($nama = 'Andrie', $pekerjaan = 'mahasiswa', $umur = '20')
     {
-        echo "Halo saya $nama, saya $pekerjaan";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
 
     public function page()
     {
-        echo 'about/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
